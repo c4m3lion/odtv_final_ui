7c4ea74af7acbf39c13209ae13a05b2e526aa6ca
c4m3lion <86702015+c4m3lion@users.noreply.github.com>
2022-04-01 18:59:46 +0300

    sadas

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Shortcuts(
-      shortcuts: {
-        LogicalKeySet(LogicalKeyboardKey.select): const ActivateIntent(),
+      shortcuts: const {
+        //LogicalKeySet(LogicalKeyboardKey.select): const ActivateIntent(),
         //LogicalKeySet(LogicalKeyboardKey.enter): ActivateIntent(),
       },
       child: MaterialApp(
